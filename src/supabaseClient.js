f5c4f0da8b38ae6eda84aabd63b96de2dbdbfd72
Rishi Prasad <rishiprasad@Rishis-MacBook-Air.local>
2026-03-03 18:22:07 +0530

    RLS permssion give

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// HARDCODE THESE JUST FOR THIS TEST
+const supabaseUrl = 'https://gbttxpvdtgmyjkfydcim.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdidHR4cHZkdGdteWprZnlkY2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NDE0NjksImV4cCI6MjA4NzUxNzQ2OX0.05Ax9SmEMsdDzp1P8Vl4-b9Oi0nI6wf1Cpl07Lz1nBQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
